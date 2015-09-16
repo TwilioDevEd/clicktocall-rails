7b57015deab903cd2a9c614b5b4641555eb25714
Phil Nash <philnash@gmail.com>
2015-09-16 10:19:22 +0100

    Tests successful call to connect

--- test/controllers/twilio_controller_test.rb
+++ test/controllers/twilio_controller_test.rb
@@ -8,8 +8,23 @@ class TwilioControllerTest < ActionController::TestCase
   end
 
   test "should fail as fake Twilio request" do
-    post(:connect, {'from' => "15008675309", 'to' => '12066505813'}, {'fake-signature' => "HpS7PBa1Agvt4OtO+wZp75IuQa0="})
+    @request.env['HTTP_X_TWILIO_SIGNATURE'] = "FAKE_SIGNATURE"
+    post :connect, 'from' => '15008675309', 'to' => '12066505813'
     assert_response 401 #Unauthorized
+  end
+
+  test "should succeed with real Twilio request" do
+    # Mock the validator so that we don't have to use a real signature here.
+    validator = Minitest::Mock.new
+    validator.expect(:validate, true, [String, Hash, String])
+    Twilio::Util::RequestValidator.stub(:new, validator) do
+      @request.env['HTTP_X_TWILIO_SIGNATURE'] = "REAL_SIGNATURE"
+      post :connect, 'from' => '15008675309', 'to' => '12066505813'
+
+      assert_response :ok
+      assert response.body.match(/<Say voice="alice">/)
+    end
+    validator.verify
   end
 
 end
