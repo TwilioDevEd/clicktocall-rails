9fdf7d0281334ffa218d383eb01b90168a7e1918
Hector Ortega <hector.oj@gmail.com>
2016-12-14 14:56:08 -0500

    Add new form field for sales phone number

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,6 +1,6 @@
 // Execute JavaScript on page load
 $(function() {
-    $('#phoneNumber').intlTelInput({
+    $('#userNumber, #salesNumber').intlTelInput({
         responsiveDropdown: true,
         autoFormat: true,
         utilsScript: 'assets/intl-phone/libphonenumber/build/utils.js'
