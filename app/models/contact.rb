2d8c62dd9a7150da361d2de462d3fb6bc75733d4
Hector Ortega <hector.oj@gmail.com>
2016-12-15 12:35:00 -0500

    Attach sales phone number to callback URL and use it to dial on returned TwiML

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,7 +1,14 @@
 # Simple model to handle validation
 class Contact
   include ActiveModel::Validations
-  attr_accessor :phone
-  validates_presence_of :phone
-  validates :phone, :phony_plausible => true
+  attr_accessor :user_phone
+  attr_accessor :sales_phone
+  validates_presence_of :user_phone
+  validates_presence_of :sales_phone
+  validates :user_phone, :phony_plausible => true
+  validates :sales_phone, :phony_plausible => true
+
+  def encoded_sales_phone
+    URI.encode(sales_phone)
+  end
 end
